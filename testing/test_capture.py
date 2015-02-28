f9aa885ba6b039c1a886223a499c9e054f5b2751
Netanel Revah <netanelrevah@users.noreply.github.com>
2015-02-28 19:52:06 +0200

    add tested dumps option

--- testing/test_capture.py
+++ testing/test_capture.py
@@ -9,7 +9,7 @@ MAGIC_WITH_SWAPPED_ORDERING = '\xd4\xc3\xb2\xa1'
 CAP_HEADER = "\xD4\xC3\xB2\xA1\x02\x00\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04\x00\x01\x00\x00\x00"
 
 
-def test_load_empty_cap():
+def test_loads_empty_cap():
     cap = capture.loads(CAP_HEADER)
     assert cap.swapped_order is True
     assert cap.version == (2, 4)
@@ -30,3 +30,10 @@ def test_create_new_capture_file():
     assert cap.time_zone == datetime.timedelta(hours=0)
     assert cap.max_capture_length == 123456
     assert len(cap) == 0
+
+
+def test_dumps_capture_file():
+    cap = capture.CaptureFile(swapped_order=True, version=(2, 4),
+                              link_layer_type=capture.LinkLayerHeaderTypes.ethernet,
+                              time_zone=datetime.timedelta(hours=0), max_capture_length=131072)
+    assert CAP_HEADER == capture.dumps(cap)
