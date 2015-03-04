35e404de8d89e14f3e489edb18f1df564e0c2a82
Netanel Revah <netanelrevah@users.noreply.github.com>
2015-03-04 14:41:56 +0200

    fix load cap with native order data

--- testing/test_capture.py
+++ testing/test_capture.py
@@ -12,6 +12,16 @@ CAP_HEADER_WITH_SWAPPED_ORDER = "\xD4\xC3\xB2\xA1\x02\x00\x04\x00\x00\x00\x00\x0
 
 
 def test_loads_empty_cap():
+    cap = capture.loads(CAP_HEADER)
+    assert cap.swapped_order is False
+    assert cap.version == (2, 4)
+    assert cap.link_layer_type == capture.LinkLayerHeaderTypes.ethernet
+    assert cap.time_zone == datetime.timedelta(hours=0)
+    assert cap.max_capture_length == 131072
+    assert len(cap) == 0
+    pass
+
+def test_loads_empty_cap_with_big_endian():
     cap = capture.loads(CAP_HEADER_WITH_SWAPPED_ORDER)
     assert cap.swapped_order is True
     assert cap.version == (2, 4)
