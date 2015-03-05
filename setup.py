bdbba1c8ad02564668086d1473a732f9062a5325
Netanel Revah <netanelrevah@users.noreply.github.com>
2015-03-05 12:13:01 +0200

    add enum requirement

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 
+REQUIREMENTS = ['enum']
+
 setup(
     name='cap',
     version='0.0.1',
     py_modules=['cap'],
+    requires=REQUIREMENTS,
     url='https://github.com/code-museum/capture',
     license='GNU General Public License, version 2',
     author='code-museum',
