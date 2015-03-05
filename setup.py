b6d01ce6cd67d1ec14d162cb4f2b7d5a5c2bcb72
Netanel Revah <netanelrevah@outlook.com>
2015-03-05 11:43:36 +0200

    change the name of the package :(

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='cap',
     version='0.0.1',
-    py_modules=['capture'],
+    py_modules=['cap'],
     url='https://github.com/code-museum/capture',
     license='GNU General Public License, version 2',
     author='code-museum',
