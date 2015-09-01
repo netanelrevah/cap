db5e5ed64f5cec9b93c8238fffce8095b05269e1
Netanel Revah <netanelrevah@users.noreply.github.com>
2015-09-01 23:31:10 +0300

    refactor more

--- cap/nicer/structs.py
+++ cap/nicer/structs.py
@@ -1,5 +1,25 @@
-__author__ = ''
+__author__ = 'netanelrevah'
 
 
-class NamedStruct(object):
-    pass
+class DefinedStruct(object):
+    NATIVE_ORDER_HEADER_STRUCT = None
+    SWAPPED_ORDER_HEADER_STRUCT = None
+
+    @classmethod
+    def get_struct(cls, is_native_order=True):
+        return cls.NATIVE_ORDER_HEADER_STRUCT if is_native_order else cls.SWAPPED_ORDER_HEADER_STRUCT
+
+    @staticmethod
+    def _filter_constants(values):
+        return values
+
+    def _get_values_tuple(self):
+        raise NotImplementedError()
+
+    def pack(self, is_native_order=True):
+        return self.get_struct(is_native_order).pack(*self._get_values_tuple())
+
+    @classmethod
+    def unpack(cls, data, is_native_order=True):
+        header_struct = cls.get_struct(is_native_order)
+        return cls(*cls._filter_constants(header_struct.unpack(data)))
--- cap/structure.py
+++ cap/structure.py
@@ -1,31 +1,9 @@
+from cap.nicer.structs import DefinedStruct
+
 __author__ = 'netanelrevah'
 
 from struct import Struct
 from cap.nicer.times import datetime_from_seconds_and_microseconds
-
-
-class DefinedStruct(object):
-    NATIVE_ORDER_HEADER_STRUCT = None
-    SWAPPED_ORDER_HEADER_STRUCT = None
-
-    @classmethod
-    def get_struct(cls, is_native_order=True):
-        return cls.NATIVE_ORDER_HEADER_STRUCT if is_native_order else cls.SWAPPED_ORDER_HEADER_STRUCT
-
-    @staticmethod
-    def _filter_constants(values):
-        return values
-
-    def _get_values_tuple(self):
-        raise NotImplementedError()
-
-    def pack(self, is_native_order=True):
-        return self.get_struct(is_native_order).pack(*self._get_values_tuple())
-
-    @classmethod
-    def unpack(cls, data, is_native_order=True):
-        header_struct = cls.get_struct(is_native_order)
-        return cls(*cls._filter_constants(header_struct.unpack(data)))
 
 
 class CapturedPacketHeaderStruct(DefinedStruct):
