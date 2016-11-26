6f5bc37014c3d443afecfab498d028ee6dd0391d
Netanel Revah <netanelrevah@outlook.com>
2016-11-26 17:42:50 +0200

    maybe it's better

--- cap/__init__.py
+++ cap/__init__.py
@@ -1,6 +1,6 @@
-from cap.version import __version__
-from cap.api import loads, dump_into_bytes, load, dump_into_file
-from cap.core import LinkLayerTypes
-from cap.core import NetworkCapture, CapturedPacket
+from .version import __version__
+from .api import loads, dump_into_bytes, load, dump_into_file
+from .core import LinkLayerTypes
+from .core import NetworkCapture, CapturedPacket
 
 __author__ = 'netanelrevah'
